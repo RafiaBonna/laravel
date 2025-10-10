6ec4a3902abb46024ef1154376e4a95dd42fbe7e
RafiaBonna <rafiaakterbonna@gmail.com>
2025-10-10 23:54:30 +0600

    my

--- dashboard_project/resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="app-footer">
-  <div class="float-end d-none d-sm-inline">Anything you want</div>
-  <strong>
-    Copyright &copy; 2014-{{ date('Y') }}&nbsp;
-    <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
-  </strong>
-  All rights reserved.
-  </footer>
