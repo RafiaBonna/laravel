8531bf6913f7fcd57f49c1cd8a9f1ab579d6073a
RafiaBonna <rafiaakterbonna@gmail.com>
2025-11-08 23:49:29 +0600

    my

--- auth-one/routes/superadmin.php
+++ auth-one/routes/superadmin.php
@@ -18,7 +18,7 @@ Route::prefix('superadmin')->group(function () {
         // ADMINISTRATION
         Route::resource('/users', UserController::class)->names('superadmin.users');
 
-        // --- INVENTORY / SETTINGS ---
+        // --- SETTINGS ---
         // ✅ Supplier Management Resource Route
         // আমরা Settings মেনুর অধীনে রাখলেও রুট-এর নাম সহজ রাখছি: superadmin.suppliers.*
         Route::resource('/suppliers', SupplierController::class)->names('superadmin.suppliers'); 
